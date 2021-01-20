8795ec9c4273636bda81af9b0375765de7df4476
Metater <metaterplays@gmail.com>
2021-01-19 22:28:11 -0500

    Added motor controller can ids to constants

--- FRC2021/src/main/java/frc/robot/Constants.java
+++ FRC2021/src/main/java/frc/robot/Constants.java
@@ -19,25 +19,46 @@ public final class Constants {
 
     public static final double DRIVE_SPEED_CUTOFF = 0.1;
 
-    public final class Buttons {
+    public final class Buttons
+    {
+        public static final int SCISSOR_LIFT_LEFT = 6;
+        public static final int SCISSOR_LIFT_RIGHT = 8;
 
-    public static final int SCISSOR_LIFT_RIGHT = 8;
-    public static final int SCISSOR_LIFT_LEFT = 6;
+        public static final int BALL_INTAKE = 1;
 
-    public static final int BALL_INTAKE = 1;
+        public static final int LIFT_COLOR_WHEEL = 4;
+        public static final int SPIN_COLOR_WHEEL = 12;
 
-    public static final int LIFT_COLOR_WHEEL = 4;
-    public static final int SPIN_COLOR_WHEEL = 12;
+        public static final int SWITCH_FRONT = 7;
 
-    public static final int SWITCH_FRONT = 7;
-
-    public static final int BLEH = 5;
+        public static final int BLEH = 5;
     }
 
-    public final class USB {
+    public final class USB
+    {
         public static final int JOYSTICK_DRIVE_LEFT = 1;
         public static final int JOYSTICK_DRIVE_RIGHT = 2;
         public static final int JOYSTICK_MANIPULATOR = 3;
+    }
+
+    public final class Motors
+    {
+        // Drive Motors
+        public static final int DRIVE_LEFT_FRONT = 1; // TalonSRX
+        public static final int DRIVE_RIGHT_FRONT = 2; // TalonSRX
+        public static final int DRIVE_LEFT_FOLLOWER = 3; // TalonSRX
+        public static final int DRIVE_RIGHT_FOLLOWER = 4; // TalonSRX
+
+        // Ball System Motors
+        public static final int INTAKE_BELT_FRONT = 5; // TalonSRX
+        public static final int INTAKE_BELT_REAR = 10; // TalonFX
+        public static final int INTAKE_WHEEL_SPINNER = 8; // TalonSRX
+
+        // Color Sensor System Motors
+        public static final int COLOR_WHEEL_SPINNER = 6; // TalonSRX
+
+        // Winch Motor
+        public static final int WINCH = 9;
     }
 
 

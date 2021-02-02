42b442f311108afef322f058aafe29a94a08bf2f
LaptopSix3489 <77548502+LaptopSix3489@users.noreply.github.com>
2021-02-01 19:44:45 -0500

    auto

--- FRC2021.1/src/main/java/frc/robot/auto/AutoInstruction.java
+++ FRC2021.1/src/main/java/frc/robot/auto/AutoInstruction.java
@@ -4,7 +4,8 @@ import edu.wpi.first.wpilibj.Timer;
 
 public class AutoInstruction {
 
-   // public Instruction
+    public InstructionType instructionType;
+    public double[] values;
     public enum InstructionType
     {
         Wait,
@@ -12,7 +13,54 @@ public class AutoInstruction {
         TurnLeft,
         TurnRight
     }
-    public AutoInstruction(int a, int b)
+
+    public AutoInstruction(InstructionType instructionType, double[] values)
     {
+        this.instructionType = instructionType;
+        this.values = values;
+    }
+
+    public void cycle()
+    {
+        switch(instructionType)
+        {
+            case Wait:
+                waitInstruction(values[0]);
+                break;
+            case Drive:
+                driveInstruction();
+                break;
+            case TurnLeft:
+                turnLeftInstruction();
+                break;
+            case TurnRight:
+                turnRightInstruction();
+                break;
+            default:
+                break;
+        }
+    }
+    private boolean isWaitInit = false;
+    private double waitFinishTime;
+    public void waitInstruction(double waitTime)
+    {
+        if (!isWaitInit)
+        {
+            isWaitInit = true;
+            waitFinishTime = Timer.getFPGATimestamp() + waitTime;
+        }
+        if ()
+    }
+    public void driveInstruction()
+    {
+
+    }
+    public void turnLeftInstruction()
+    {
+
+    }
+    public void turnRightInstruction()
+    {
+
     }
 }
--- FRC2021.1/src/main/java/frc/robot/general/AutoHandler.java
+++ FRC2021.1/src/main/java/frc/robot/general/AutoHandler.java
@@ -7,73 +7,9 @@ import frc.robot.auto.*;
 public class AutoHandler {
 
     private RobotHandler robotHandler;
-
-    private double startClicks = 0;
-    private double stopClicks = 0;
-
-    private boolean hasSetClicks = false;
-
-    private int step = 0;
-
-    /*
-    private ArrayList<AutoInstruction> slalom =v
-    {
-        new AutoInstruction(),
-        new AutoInstruction(),
-        new AutoInstruction()
-    };
-    */
-
+    
     public AutoHandler(RobotHandler robotHandler)
     {
         this.robotHandler = robotHandler;
-    }
-
-    public void autonomousInit()
-    {
-        robotHandler.driveHandler.differentialDrive.setSafetyEnabled(false);
-    }
-
-    public void autonomousPeriodic()
-    {
-        robotHandler.driveHandler.differentialDrive.setSafetyEnabled(false);
-
-        switch (step) {
-            case 0:
-                tankDriveClicks(0.5, 0.5, 20);
-                break;
-
-            case 1:
-                stopTankDrive();
-                break;
-        }
-    }
-
-    public void stopTankDrive()
-    {
-        robotHandler.driveHandler.differentialDrive.tankDrive(0, 0);
-        step++;
-    }
-
-    public void tankDriveClicks(double leftSpeed, double rightSpeed, double clicks)
-    {
-        if (!hasSetClicks)
-        {
-            startClicks = robotHandler.driveHandler._leftFront.getSelectedSensorPosition();
-            stopClicks = robotHandler.driveHandler._leftFront.getSelectedSensorPosition() + clicks; // Collides low to high number
-            hasSetClicks = true;
-            System.out.println("Set stop time");
-        }
-        if (robotHandler.driveHandler._leftFront.getSelectedSensorPosition() <= stopClicks)
-        {
-            robotHandler.driveHandler.differentialDrive.tankDrive(leftSpeed, rightSpeed);
-            System.out.println("Driving, on step: " + step);
-        }
-        else
-        {
-            step++;
-            hasSetClicks = false;
-            System.out.println("Finished, increment step");
-        }
     }
 }

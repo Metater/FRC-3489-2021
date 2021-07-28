58472c0e0482e91f7254020431f292ae5a3055e5
Connor <61102529+Metater@users.noreply.github.com>
2021-07-28 18:21:18 -0400

    Start on autonomous engine

--- FRC-3489-2021-NewRobot/src/main/java/frc/robot/auto/AutoInterpreter.java
+++ FRC-3489-2021-NewRobot/src/main/java/frc/robot/auto/AutoInterpreter.java
@@ -2,14 +2,20 @@ package frc.robot.auto;
 
 import java.util.List;
 
+import frc.robot.handlers.AutoHandler;
 import frc.robot.shared.utils.FileUtils;
 
 public class AutoInterpreter {
 
+    private AutoHandler autoHandler;
     private List<AutoInstruction> instructions;
 
-    public AutoInterpreter(String autoFileName)
+    private int currentInstruction = 0;
+
+    public AutoInterpreter(AutoHandler autoHandler, String autoFileName)
     {
+        this.autoHandler = autoHandler;
+
         System.out.println("Start auto load, lex and parse");
         List<String> codeLines = FileUtils.readLocalFile(autoFileName + ".auto");
         AutoParser autoParser = new AutoParser(codeLines);

80b8db0f8f6db85ebc1ecbda471582dce1dde36c
Connor <61102529+Metater@users.noreply.github.com>
2021-10-09 12:51:37 -0400

    swipe slower, and no speed curve

--- FRC-3489-2021-NewRobot/src/main/java/frc/robot/CarnivalGame.java
+++ FRC-3489-2021-NewRobot/src/main/java/frc/robot/CarnivalGame.java
@@ -135,16 +135,19 @@ public class CarnivalGame {
         switch (difficulty)
         {
             case 0:
+                y = -0.01;
+                break;
+            case 1:
                 //y = (1.9 * Math.pow(x - 0.2, 2)) - (1.2 * Math.pow(x, 3)) + 0.0139;
                 y = (-3 * Math.pow(x, 2)) + 0.07;
                 //y = 0.1;
                 break;
-            case 1:
+            case 2:
                 //y = (2 * Math.pow(x - 0.2, 2)) - (1.2 * Math.pow(x, 3)) + 0.0139;
                 y = (-12 * Math.pow(x, 2)) + 0.09;
                 //y = 0.15;
                 break;
-            default:
+            case 3:
                 //y = (2.5 * Math.pow(x - 0.2, 2)) - (1.2 * Math.pow(x, 3)) + 0.0139;\
                 y = (-16 * Math.pow(x, 2)) + 0.11;
                 //y = 0.2;

00a19d79583b7779db7ce0d3f31035f4f86ed567
BitCounter3489 <BitCounter3489@gmail.com>
2022-01-29 15:56:32 -0500

    ShooterS gobrrrr

--- Shooter testing/src/main/java/frc/robot/Robot.java
+++ Shooter testing/src/main/java/frc/robot/Robot.java
@@ -19,102 +19,84 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
-
-  private RobotContainer m_robotContainer;
 
   private WPI_TalonFX leftFalcon = new WPI_TalonFX(15);
   private WPI_TalonFX rightFalcon = new WPI_TalonFX(16);
 
   private Joystick joystick = new Joystick(2);
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
+
   }
-
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
+
   }
 
   double zoomieness = 0;
 
-  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
+
+    if (joystick.getRawButtonPressed(3)) {
+      zoomieness -= 0.025;
+    }
+    if (joystick.getRawButtonPressed(4)) {
+      zoomieness += 0.025;
+    }
 
     if (joystick.getRawButton(1))
     {
       zoomieness = 0;
     }
-    if (joystick.getRawButton(8))
+    if (joystick.getRawButton(7))
+    {
+      zoomieness = 0.4;
+    }
+    if (joystick.getRawButton(9))
+    {
+      zoomieness = 0.5;
+    }
+    if (joystick.getRawButton(11))
     {
       zoomieness = 0.6;
     }
-    if (joystick.getRawButton(10))
+    if (joystick.getRawButton(8))
     {
       zoomieness = 0.7;
     }
+    if (joystick.getRawButton(10))
+    {
+      zoomieness = 0.8;
+    }
     if (joystick.getRawButton(12))
     {
-      zoomieness = 0.75;
+      zoomieness = 0.9;
+    }
+    if (joystick.getRawButton(2))
+    {
+      zoomieness = 0.2;
+    }
+    if (joystick.getRawButton(5))
+    {
+      zoomieness -= 0.05;
+    }
+    if (joystick.getRawButton(6))
+    {
+      zoomieness += 0.05;
     }
 
     double y = joystick.getY();
     setShooter(zoomieness);
     System.out.println("Speed: " + ((int)(zoomieness * 1000))/10d);
-    System.out.println(leftFalcon.getSelectedSensorVelocity());
+    System.out.println(leftFalcon.getSelectedSensorVelocity() / -2048d);
     if (Math.abs(y) < 0.1) return;
     zoomieness += y * -0.02;
   }
@@ -124,14 +106,4 @@ public class Robot extends TimedRobot {
     leftFalcon.set(-speed);
     rightFalcon.set(speed);
   }
-
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
 }

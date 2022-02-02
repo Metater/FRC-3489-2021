4f1e90db20a27ab28364de134d0ec698ae4b2e4f
BitCounter3489 <BitCounter3489@gmail.com>
2022-02-02 18:51:22 -0500

    e

--- FRC-3489-2021-JustDrive/src/main/java/frc/robot/Robot.java
+++ FRC-3489-2021-JustDrive/src/main/java/frc/robot/Robot.java
@@ -1,98 +1,102 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  private WPI_TalonSRX driveLeftFront = new WPI_TalonSRX(1);
+  private WPI_TalonSRX driveRightFront = new WPI_TalonSRX(2);
+  private WPI_TalonSRX driveLeftFollower = new WPI_TalonSRX(3);
+  private WPI_TalonSRX driveRightFollower = new WPI_TalonSRX(4);
+
+  private Joystick joystickDriveLeft = new Joystick(0);
+  private Joystick joystickDriveRight = new Joystick(1);
+
+  private DifferentialDrive differentialDrive;
+
+  private boolean frontSwitched = false;
+
+  private boolean shouldSwitchFront() {
+    return joystickDriveLeft.getRawButtonPressed(7) || joystickDriveRight.getRawButtonPressed(7);
+  }
+
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+
+    if (FileUtils.fileExists("frontSwitched.txt")) {
+      frontSwitched = FileUtils.readLocalFile("frontSwitched.txt").get(0) == "1";
+    }
+    else {
+      FileUtils.createLocalFile("frontSwitched.txt");
+    }
+
+    driveLeftFollower.follow(driveLeftFront);
+    driveRightFollower.follow(driveRightFront);
+    differentialDrive = new DifferentialDrive(driveLeftFront, driveRightFront);
+
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
+  }
+
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+
+  }
+
+  @Override
+  public void teleopInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    if (shouldSwitchFront()) {
+      frontSwitched = !frontSwitched;
+    }
+
+    double leftY = joystickDriveLeft.getY();
+    double rightY = joystickDriveRight.getY();
+
+    double leftSpeed = 0;
+    double rightSpeed = 0;
+
+    if (Math.abs(leftY) >= 0.1) leftSpeed = leftY;
+    if (Math.abs(rightY) >= 0.1) rightSpeed = rightY;
+
+    if (frontSwitched) {
+      differentialDrive.tankDrive(leftSpeed, rightSpeed);
+    }
+    else {
+      differentialDrive.tankDrive(-rightSpeed, -leftSpeed);
     }
   }
 
-  /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void disabledInit() {
+    String frontSwitchedText = frontSwitched ? "1" : "0";
+    FileUtils.writeLocalFile("frontSwitched.txt", frontSwitchedText);
+  }
 
-  /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void disabledPeriodic() {
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
+  }
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This function is called once when test mode is enabled. */
   @Override
   public void testInit() {}
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 }

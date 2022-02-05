e5b17fd83ae3f248dbff3ddb32fffc82461017b2
BitCounter3489 <BitCounter3489@gmail.com>
2022-02-05 10:17:38 -0500

    change switch front buttons

--- FRC-3489-2021-JustDrive/src/main/java/frc/robot/Robot.java
+++ FRC-3489-2021-JustDrive/src/main/java/frc/robot/Robot.java
@@ -21,7 +21,7 @@ public class Robot extends TimedRobot {
   private boolean frontSwitched = false;
 
   private boolean shouldSwitchFront() {
-    return joystickDriveLeft.getRawButtonPressed(14) || joystickDriveRight.getRawButtonPressed(13);
+    return joystickDriveLeft.getRawButtonPressed(13) || joystickDriveRight.getRawButtonPressed(13);
   }
 
   @Override
